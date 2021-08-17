4abac8a483c06c6530f8ced41f690a08284a0e81
Michael Peters <michaelpeters5647@gmail.com>
2021-08-17 22:14:20 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,58 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { LightningBoltIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-black dark:text-gray-500">
       <Head>
-        <title>Create Next App</title>
+        <title>Discord Bot Creator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <h2 className="text-5xl font-semibold dark:text-gray-500 mb-3">
+          Discord Bot Creator
+        </h2>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className="mt-3 text-xl">
+          Get started by downloading{" "}
+          <a className="bg-blue-700 hover:bg-blue-600 rounded-md p-2 pt-1 text-white">
+            our program
+          </a>
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+        <div className="mt-10">
+          <h3 className="text-2xl font-semibold">Modules</h3>
+          {/* Create the cards for with the three titles from above plus some additional content */}
+          <div className="border p-5 rounded-xl border-gray-500 text-left">
+            <h4 className="text-xl font-semibold mb-2">Welcome Module</h4>
+            <p>
+              Greets the user when they join the server and gives them a role.
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+          </div>
+          <div>
+            <h4>Moderation</h4>
+            <p>
+              Manages the server and gives users special roles and permissions.
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
+          <div>
+            <h4>Leveling System</h4>
+            <p>Manages the user's experience and level.</p>
+          </div>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 border-t dark:border-gray-600">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <LightningBoltIcon className="w-4 mr-1 text-blue-600 transition-all hover:text-blue-400" />
+          Powered by Discord Bot Creator
         </a>
       </footer>
     </div>
-  )
+  );
 }
