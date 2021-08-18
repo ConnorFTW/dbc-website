0e4e3d2cadbc7ebdea31df8ebba6f4b8f44fd244
Michael Peters <michaelpeters5647@gmail.com>
2021-08-18 16:55:10 +0200

    Add button component

---
+++ components/Button.js
@@ -0,0 +1,3 @@
+export default function Button(props) {
+  return <button className="bg-blue-800 hover:bg-blue-900" {...props} />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,63 @@
 import Head from "next/head";
 import { LightningBoltIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import Button from "../components/Button";
+
+const list = [
+  {
+    title: "Welcome Module",
+    description:
+      "Greets the user when they join the server and gives them a role.",
+  },
+  {
+    title: "Moderation",
+    description:
+      "Manages the server and gives users special roles and permissions.",
+  },
+  {
+    title: "Leveling System",
+    description: "Manages the user's experience and level.",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-black dark:text-gray-500">
+    <div className="flex flex-col items-center justify-center min-h-screen dark:bg-black dark:text-gray-500">
       <Head>
         <title>Discord Bot Creator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="border-b-2 border-gray-700 w-full p-3">
+        <h4 className="font-semibold text-xl text-gray-200">DBC</h4>
+      </div>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h2 className="text-5xl font-semibold dark:text-gray-500 mb-3">
+        <h2 className="text-5xl font-semibold dark:text-gray-300 mb-3">
           Discord Bot Creator
         </h2>
 
         <p className="mt-3 text-xl">
-          Get started by downloading{" "}
-          <a className="bg-blue-700 hover:bg-blue-600 rounded-md p-2 pt-1 text-white">
-            our program
-          </a>
+          Get started by downloading <Button>our program</Button>
         </p>
 
         <div className="mt-10">
-          <h3 className="text-2xl font-semibold">Modules</h3>
+          <h3 className="text-2xl font-semibold dark:text-white mb-3">
+            Modules
+          </h3>
           {/* Create the cards for with the three titles from above plus some additional content */}
-          <div className="border p-5 rounded-xl border-gray-500 text-left">
-            <h4 className="text-xl font-semibold mb-2">Welcome Module</h4>
-            <p>
-              Greets the user when they join the server and gives them a role.
-            </p>
-          </div>
-          <div>
-            <h4>Moderation</h4>
-            <p>
-              Manages the server and gives users special roles and permissions.
-            </p>
-          </div>
-          <div>
-            <h4>Leveling System</h4>
-            <p>Manages the user's experience and level.</p>
-          </div>
+          {list.map((item) => (
+            <div className="p-5 rounded-xl text-left mb-4 transition hover:bg-gray-800 dark:bg-gray-900">
+              <h4 className="text-xl font-semibold mb-2 dark:text-gray-200">
+                {item.title}
+              </h4>
+              <p>{item.description}</p>
+            </div>
+          ))}
+          <Link href="/hubs/modules">
+            <button className="bg-blue-700 px-5 py-3 text-white text-lg rounded-lg font-semibold hover:bg-blue-800 active:bg-blue-900">
+              See more
+            </button>
+          </Link>
         </div>
       </main>
 
